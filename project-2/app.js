d9af0e0cd9631119b03cadc8946043f3b574b4e4
Hazal <88984994+xgravellx@users.noreply.github.com>
2022-05-29 12:53:09 +0300

    second commit

--- project-2/app.js
+++ project-2/app.js
@@ -1,36 +1,56 @@
 // arayüz elementleri seçmek
-const name = document.getElementById('name');
-const surname = document.getElementById('surname');
-const email = document.getElementById('mail');
+const ad = document.getElementById('name');
+const soyad = document.getElementById('surname');
+const mail = document.getElementById('mail');
 
 const form = document.getElementById('form-guide');
+const kisiListesi = document.querySelector('.kisi-listesi');
 
 // Event Listenerların tanımlanması
 form.addEventListener('submit', save);
+
+// tüm kişiler için dizi 
+const tumKisilerDizisi = [];
 
 function save(e) {
     e.preventDefault();
 
     const eklenecekKisi = {
-        name:name.value,
-        surname: surname.value,
-        email:email.value,
+        ad:ad.value,
+        soyad: soyad.value,
+        mail:mail.value,
     }
 
     const sonuc = verileriKontrolEt(eklenecekKisi);
     if(sonuc.durum) {
-        console.log('Sıkıntı yok');
+        kisiyiEkle(eklenecekKisi);
     }
     else {
         bilgiOlustur(sonuc.mesaj, sonuc.durum);
-        console.log(sonuc.mesaj);
     }
 
     //console.log(eklenecekKisi);
 }
 
+function kisiyiEkle(eklenecekKisi) {
+    const olusturulanTrElementi = document.createElement('tr');
+    olusturulanTrElementi.innerHTML = `
+    <td>${eklenecekKisi.ad}</td>
+    <td>${eklenecekKisi.soyad}</td>
+    <td>${eklenecekKisi.mail}</td>
+    <td>
+        <button class="btn btn-edit"><i class="fa-solid fa-pen-to-square"></i></button> 
+        <button class="btn btn-trash"><i class="fa-solid fa-trash"></i></button> 
+    </td>
+    `;
+    kisiListesi.appendChild(olusturulanTrElementi);
+    tumKisilerDizisi.push(eklenecekKisi);
+
+    bilgiOlustur('Kişi Rehbere kaydedildi.', true);
+}
+
 function verileriKontrolEt(person) {
-    // object in kullanımı
+    // objectlerde in kullanımı
     for (const deger in person) {
         if (person[deger]) {
             console.log(person[deger]);
@@ -43,9 +63,10 @@ function verileriKontrolEt(person) {
             return sonuc;
         }
     }
+    alanlariTemizle();
     return {
         durum: true,
-        mesaj: '',
+        mesaj: 'kaydedildi',
     }
 }
 
@@ -53,13 +74,27 @@ function bilgiOlustur(mesaj, durum) {
     const olusturulanBilgi = document.createElement('div');
     olusturulanBilgi.textContent = mesaj;
     olusturulanBilgi.className = 'info';
-    if (durum) {
+    /* if (durum) {
         olusturulanBilgi.classList.add('info-success');
     }
     else {
         olusturulanBilgi.classList.add('info-error');
-    }
+    } */
+    olusturulanBilgi.classList.add(durum ? 'info-success' : 'info-error');
 
     document.querySelector('.container').insertBefore(olusturulanBilgi, form);
+    
+    // setTimeOut, setInterval
+    setTimeout(function() {
+        const silinecekDiv = document.querySelector('.info');
+        if(silinecekDiv) {
+            silinecekDiv.remove();
+        }
+    }, 2000);
+}
 
+function alanlariTemizle() {
+    ad.value = '';
+    soyad.value = '';
+    mail.value = '';
 }
